87871ba2989572dc9573933785b384bb70cdc2d8
emerson-kolarevic <ekolarevic@gmail.com>
2025-04-08 19:06:41 -0300

    primeiro commit

---
+++ 1050.py
@@ -0,0 +1,8 @@
+cidades = {61: "Brasilia", 71: "Salvador", 11: "Sao Paulo", 21: "Rio de Janeiro", 32: "Juiz de Fora", 19: "Campinas", 27: "Vitoria", 31: "Belo Horizonte"}
+
+ddd = int(input())
+
+if ddd not in cidades:
+    print("DDD nao cadastrado")
+else:
+    print(cidades[ddd])
