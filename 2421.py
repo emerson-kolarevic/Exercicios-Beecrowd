74cc47cd2b40596eaa0f6fb49bdb41f32d1e9593
emerson-kolarevic <ekolarevic@gmail.com>
2025-04-12 14:31:57 -0300

    add_2421

--- 2421.py
+++ 2421.py
@@ -1,10 +1,23 @@
-aa, ll = map(int, input().split())
-l1, a1 = map(int, input().split())
-a2, l2 = map(int, input().split())
+x, y = map(int, input().split())
+l1, h1 = map(int, input().split())
+l2, h2 = map(int, input().split())
 
-if a1 + a2 <=aa:
-    if l1 <= ll:
-        if l2 <= ll:
-            print("S")
-else:
-    print("N")
+def cabe(x, y, l1, h1, l2, h2):
+    # lado a lado
+    if(l1 + l2 <= x and max(h1, h2) <= y):
+        return True
+    # Empilhadas
+    if(h1 + h2 <= y and max(l1, l2) <= x):
+        return True
+    return False
+
+# Testando todas as rotações possíveis das fotos
+resultado = (
+    cabe(x, y, l1, h1, l2, h2) or
+    cabe(x, y, h1, l1, l2, h2) or
+    cabe(x, y, l1, h1, h2, l2) or
+    cabe(x, y, h1, l1, h2, l2)
+)
+
+# Impriome resposta
+print("S" if resultado else "N")
