7c84a0acfacebe465cf4abbe8c1c65fa9dbf51ba
emerson-kolarevic <ekolarevic@gmail.com>
2025-04-23 18:03:16 -0300

    add_1175

---
+++ 1175.py
@@ -0,0 +1,13 @@
+N = []
+
+for i in range(20):
+    valor = int(input())
+    N.append(valor)
+
+for i in range(10):
+    temp = N[i]
+    N[i] = N[19 - i]
+    N[19 -i] = temp
+
+for i in range(20):
+    print(f"N[{i}] = {N[i]}")
