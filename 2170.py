8e7b57e7123a01f301685493820683e50bff1681
emerson-kolarevic <ekolarevic@gmail.com>
2025-04-28 16:17:34 -0300

    add_2170

--- 2170.py
+++ 2170.py
@@ -1,16 +1,13 @@
-X1, Y1 = map(int, input().split())
-X2, Y2 = map(int, input().split())
-X3, Y3 = map(int, input().split())
+i = 1
 
-juros1 = ((Y1/X1)-1)*100
-juros2 = ((Y2/X2)-1)*100
-juros3 = ((Y3/X3)-1)*100
+while True:
+    try:
+       X1, Y1 = map(float, input().split())
 
-print("\nProjeto 1:")
-print(f"Percentual dos juros da aplicacao: {juros1:.2f} %\n")
+       juros = ((Y1/X1)-1)*100
 
-print("Projeto 2:")
-print(f"Percentual dos juros da aplicacao: {juros2:.2f} %\n")
+       print(f"Projeto {i}:")
+       print(f"Percentual dos juros da aplicacao: {juros:.2f} %\n")
 
-print("Projeto 3:")
-print(f"Percentual dos juros da aplicacao: {juros3:.2f} %\n")
+    except EOFError:
+        break
